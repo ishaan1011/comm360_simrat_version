5d09a741d2a45db3bf43de9657041861fbb96093
ishaan1011 <110586493+ishaan1011@users.noreply.github.com>
2025-06-20 12:37:04 -0700

    Pushing - Running on Mine Now

--- secure_room_talk/src/main.tsx
+++ secure_room_talk/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId="947647702988-24pklfpf01j1ov3n1hiusd93ehkr454a.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="775095815009-q4nd4nial7e3g12aa4nkmimtlsd6a8g0.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
 );
